970dd30706209b198aa38278847ba21c6f05e62b
Fabio Leal <sousaleal.fabio@gmail.com>
2012-10-22 09:47:16 -0200

    changing sqlite path

--- tcg_web/settings.py
+++ tcg_web/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'C:\\Users\\fabiosl\\workspace\\tcg\\tcg_sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': 'tcg_sqlite.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
