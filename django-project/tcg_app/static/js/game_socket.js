709b490b8572286877e96ba0ad20e2bbc0155939
Fabio Leal <sousaleal.fabio@gmail.com>
2013-01-21 12:22:05 -0300

    Created GameSocket JS Class

---
+++ django-project/tcg_app/static/js/game_socket.js
@@ -0,0 +1,39 @@
+var GameSocket = (function() {
+    // "private" variables 
+    var _socket;
+    
+    // constructor
+    function GameSocket(host){
+    	_socket = io.connect(host)
+		_socket.on('connect', notifyChat_UI);
+		_socket.on('updateLoggedUsers', updateLoggedUsers_UI);
+		_socket.on('roomCreation', roomCreation_UI)
+		_socket.on('chatMessage', writeMessageOnChat_UI);
+		_socket.on('reconnect',reconnect_UI);
+		_socket.on('reconnecting', reconnecting_UI);
+		_socket.on('error', error_UI);
+		
+    };
+
+	GameSocket.prototype.connect = function(user_id,password) {
+		_socket.emit('establishConnection', user_id, password,  function(set) {
+			clear_UI();
+			return $('#chat').addClass('nickname-set');
+	    });
+    };
+    
+	GameSocket.prototype.worker = function() {
+        _socket.emit('ping', 'ping');
+        setTimeout(GameSocket.prototype.worker, 30000);
+    };
+    
+    GameSocket.prototype.sendCreateRoomSignal = function(roomName){
+        _socket.emit('createRoom', roomName);
+    };
+    
+    GameSocket.prototype.sendChatMessage = function(message){
+    	_socket.emit('chatMessage', message);
+    };
+    
+    return GameSocket;
+})();
